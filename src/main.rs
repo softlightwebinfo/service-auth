5f7d9bd44d8dd5ac0c45f67057f24e5ca0d4c51e
Rafael González Muñoz <rafael.gonzalez.1737@gmail.com>
2021-04-02 19:21:42 +0200

    ADD database connect

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 use actix_web::{App, HttpServer, middleware};
+use actix_web::middleware::Logger;
+use diesel::PgConnection;
+use diesel::r2d2::ConnectionManager;
+use env_logger::Env;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 use crate::controllers::index::index;
 use crate::controllers::steam::stream;
 use crate::state::app_state::AppState;
-use actix_web::middleware::Logger;
-use env_logger::Env;
-use openssl::ssl::{SslFiletype, SslAcceptor, SslMethod};
 
 mod controllers;
 mod state;
+mod models;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: models::Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     builder
         .set_private_key_file("key.pem", SslFiletype::PEM)
         .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
-    HttpServer::new(|| {
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .data(AppState {
                 app_name: String::from("Actix-web"),
             })
---
+++ src/models.rs
@@ -0,0 +1,5 @@
+// models.rs
+use diesel::{r2d2::ConnectionManager, PgConnection};
+
+// type alias to use in multiple places
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
